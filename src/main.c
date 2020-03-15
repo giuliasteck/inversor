cb3da292503912383f85fa72dd3f0288ec6f77bf
giuliasteck <steckmarco@outlook.com>
2020-03-15 03:15:40 +0000

    Refazer

--- src/main.c
+++ src/main.c
@@ -11,29 +11,6 @@ const int tam_buffer=100;
 
 void reverse(char *buffer){
 	
-	char tmp;
-	int i, j, k, len;
-	len = strlen(buffer);
-
-	for(i=0, j =0; j<len; j++){
-		if (!isalnum(buffer[j])|| (j == len-1)){
-			if (j<len-1){
-				k = j-1;
-			}else{
-				k = j;
-			}
-
-		
-		while (i<k){
-			tmp = buffer[i];
-			buffer[i] = buffer[k];
-			buffer[k] = tmp;
-			i++;
-			k--;
-		}
-
-		i = j + 1;
-	}
   }
 
 	//printf("%s", buffer);
@@ -42,7 +19,6 @@ void reverse(char *buffer){
 int main() {
   char buffer[tam_buffer];
   fgets(buffer, tam_buffer, stdin);
-  //scanf("%[^\n]", &buffer);
   reverse(buffer);
   printf("%s", buffer);
   return 0;
